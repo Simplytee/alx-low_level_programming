77acf2f7c428e90f528051bb2c5dc399bf31fa9f
Simplytee <tinyiko.mgiba14@gmail.com>
2023-08-12 03:53:43 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,15 +6,16 @@
 
 int main(void)
 {
-char c;
+        char c;
 
-c = 'a';
-while
-(c <= 'z') {
-putchar(c)
-c++;
+        c = 'a';
+        while
+             
+	     (c <= 'z') {
+                     putchar(c);
+                     c++;
 
-}
-putchar('\n');
-return (0);
+             }
+       putchar('\n');
+       return (0);
 }

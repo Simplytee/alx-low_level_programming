1e000de52ceb4728c4cfc7cc8e4054eabcd0374c
Simplytee <tinyiko.mgiba14@gmail.com>
2023-08-13 09:34:41 -0700

    print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Invariably 0 (Success)
+ */
+
+int main(void)
+{
+	char b;
+
+	int d;
+
+	b = 'a';
+	d = 0;
+	while 
+		(d < 10) {
+			putchar(d = '0');
+			d++;
+		}
+	while
+		(b <= 'f') {
+			putchar(b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}

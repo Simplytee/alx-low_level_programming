e4bd39f80dd1237f72be7f37b6d75726aca8231a
Simplytee <tinyiko.mgiba14@gmail.com>
2023-08-16 00:56:57 -0700

    function that prints every minute of the day of Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,25 +5,24 @@
  */
 
 void jack_bauer(void)
+{
+int i, j;
 
-{
+i = 0;
 
-int a, b, c, d;
-
-for (a = 0; a <= 2; a++)
+while (i < 24)
 {
-for (b = 0; b <= 9; b++)
+j = 0;
+while (j < 60)
 {
-if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
-{
-for (c = 0; c <= 5; c++)
-{
-for (d = 0; d <= 9; d++)
-{
-_putchar(a + '0');
-_putchar(b + '0');
-_putchar(58);
-_putchar(c + '0');
-_putchar(d + '0'0;
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+_putchar(':');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
 _putchar('\n');
+j++;
+}
+i++;
+}
 }
